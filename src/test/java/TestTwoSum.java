dd7afac0337d506885ddb50a67017f8c4083dd64
cherrydan <cherrydan.1978@gmail.com>
2023-02-08 14:33:29 +0300

    commit with tests

--- src/test/java/TestTwoSum.java
+++ src/test/java/TestTwoSum.java
@@ -1,2 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestTwoSum {
+
+
+    @Test
+    public void testTwoSum1() {
+        Assertions.assertArrayEquals(TwoSumSolution.twoSum(new Integer[]{ 2, 3, 4}, 6), new Integer[]{0, 2});
+    }
+
+    @Test
+    public void testTwoSum2() {
+        Assertions.assertArrayEquals(TwoSumSolution.twoSum(new Integer[]{ 2, 7, 11, 15}, 9),
+                new Integer[]{0, 1});
+    }
+
+    @Test
+    public void testTwoSum3() {
+        Assertions.assertArrayEquals(TwoSumSolution.twoSum(new Integer[]{ 2, 7, 11, 15}, 26),
+                new Integer[]{2, 3});
+    }
+
+
 }
