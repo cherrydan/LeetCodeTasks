701ba6ee80633686d5335502776b9026028af5fa
cherrydan <cherrydan.1978@gmail.com>
2023-02-08 16:48:30 +0300

    new LeetCode task with tests

--- src/test/java/TestContainer.java
+++ src/test/java/TestContainer.java
@@ -3,7 +3,12 @@ import org.junit.jupiter.api.Test;
 
 public class TestContainer {
     @Test
-    public void testContainer() {
-        Assertions.assertEquals(ContainerSolution.maxArea(new int[]{1,8,6,2,5,4,8,3,7}), 48);
+    public void testContainer1() {
+        Assertions.assertEquals(ContainerSolution.maxArea(new int[]{1,8,6,2,5,4,8,3,7}), 49);
+    }
+
+    @Test
+    public void testContainer2() {
+        Assertions.assertEquals(ContainerSolution.maxArea(new int[]{1, 1}), 1);
     }
 }
