793761fd4e2f1be480c720905738144d8690716f
cherrydan <cherrydan.1978@gmail.com>
2023-02-09 19:00:48 +0300

    LeetCode task integerToRoman digits without Hashmap with param.test with CsvSource arguments

---
+++ src/test/java/TestIntToRoman.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class TestIntToRoman {
+    @ParameterizedTest
+    @CsvSource(value = {
+
+            "3, III",
+            "58, LVIII",
+            "1994, MCMXCIV",
+            "971, CMLXX"
+
+    })
+    public void testIntToRoman(int num, String expected) {
+        Assertions.assertEquals(IntegerToRoman.intToRoman(num), expected);
+    }
+}
