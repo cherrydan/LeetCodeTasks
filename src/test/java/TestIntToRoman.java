695b2222286505937a2f2d259f3597b66fd09439
cherrydan <cherrydan.1978@gmail.com>
2023-02-09 19:24:21 +0300

    HA!!! CI GitHub skipped parametrized tests!

--- src/test/java/TestIntToRoman.java
+++ src/test/java/TestIntToRoman.java
@@ -3,17 +3,5 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class TestIntToRoman {
-    @ParameterizedTest
-    @CsvSource(value = {
 
-            "3, III",
-            "58, LVIII",
-            "1994, MCMXCIV",
-            "27, XXVII",
-            "971, CMLXXI"
-
-    })
-    public void testIntToRoman(int num, String expected) {
-        Assertions.assertEquals(IntegerToRoman.intToRoman(num), expected);
-    }
 }
--- src/test/java/TestValidBrackets.java
+++ src/test/java/TestValidBrackets.java
@@ -1,13 +1,23 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
 
 public class TestValidBrackets {
 
-    @ParameterizedTest
-    @ValueSource(strings = {"(abc)", "(({}[()]))",
-                            "()[]{}"})
-    public void testValidBrackets(String s) {
-        Assertions.assertTrue(ValidBracketsSolution.isValid(s));
+//    @ParameterizedTest
+//    @ValueSource(strings = {"(abc)", "(({}[()]))",
+//                            "()[]{}"})
+//    public void testValidBrackets(String s) {
+//        Assertions.assertTrue(ValidBracketsSolution.isValid(s));
+//    }
+
+    @Test
+    public void testValidBrackets1() {
+        Assertions.assertTrue(ValidBracketsSolution.isValid("(abc)"));
+
+    }
+
+    @Test
+    public void testValidBrackets2() {
+        Assertions.assertTrue(ValidBracketsSolution.isValid("(({}[()])"));
     }
 }
