82e24e5d484e6f1533534ec00ad00c7e83098bb4
cherrydan <cherrydan.1978@gmail.com>
2023-05-14 19:12:12 +0300

    LetterCombinatiosForPhoneNumber rewrite solution with tests

---
+++ src/test/java/TestLetterCombinations.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TestLetterCombinations {
+
+    @Test
+    public void test_two_digits() {
+       List<String> actualArray = Arrays.asList("ac","ae","af","bd","be","bf","cd","ce","cf");
+
+        Assertions.assertEquals(LetterCombinations.letterCombinations("23"), actualArray);
+    }
+
+    @Test
+    public void test_empty() {
+        List<String> actualArray = List.of();
+        Assertions.assertEquals(LetterCombinations.letterCombinations(""), actualArray);
+    }
+
+    @Test
+    public void test_one_digit() {
+        List<String> actualArray = Arrays.asList("a","b","c");
+        Assertions.assertEquals(LetterCombinations.letterCombinations("2"), actualArray);
+    }
+}
